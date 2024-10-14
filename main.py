da03ad50cac3058edaae1d21e829e3b8367274ef
Mayowa <ssshobanke@gmail.com>
2024-10-14 08:11:30 -0700

    print tracker options

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("----- Personal Budget Tracker -----")
+print("1. Add income")
+print("2. Add expense")
+print("3. View summary")
+print("4. Exit")
+
+choice = int(input("Enter your choice(1-4): "))
